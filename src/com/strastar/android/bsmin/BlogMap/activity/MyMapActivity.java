161ba01731f8c7696024e81b9b41ee0386735e91
Hyungsuk Hong <aanoaa@gmail.com>
2011-01-05 14:47:05 +0900

    added search dialog base
    refer: http://developer.android.com/guide/topics/search/search-dialog.html
    
    also, added new launcher icon set

--- src/com/strastar/android/bsmin/BlogMap/activity/MyMapActivity.java
+++ src/com/strastar/android/bsmin/BlogMap/activity/MyMapActivity.java
@@ -1,6 +1,9 @@
 package com.strastar.android.bsmin.BlogMap.activity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.maps.MapActivity;
 import com.strastar.android.bsmin.BlogMap.R;
@@ -11,11 +14,21 @@ public class MyMapActivity extends MapActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.mymap);
+        
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+        	String query = intent.getStringExtra(SearchManager.QUERY);
+        	search(query);
+        }
     }
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
+	}
+	
+	private void search(String query) {
+		Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
 	}
 }
---
+++ src/com/strastar/android/bsmin/BlogMap/provider/SearchHistoryProvider.java
@@ -0,0 +1,7 @@
+package com.strastar.android.bsmin.BlogMap.provider;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+public class SearchHistoryProvider extends SearchRecentSuggestionsProvider {
+	
+}
